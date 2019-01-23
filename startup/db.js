03cc4422beb636967dcf3cf876c8fac351d929bd
Jastria Rahmat <ijash1000@gmail.com>
2019-01-23 13:41:15 +0700

    add dependencies

---
+++ startup/db.js
@@ -0,0 +1,10 @@
+const config = require('config')
+const mongoose = require('mongoose');
+module.exports = function () {
+  const db = config.get('db')
+  // const db = config.get('dbRemote')
+  mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
+    .then(() => {
+      console.log('mongodb connected...')
+    })
+}
