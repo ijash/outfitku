d30975710ce811d46f274c22e6d1af0ca27e4ce3
Unknown <shirleyarianto24@gmail.com>
2019-01-25 17:29:07 +0700

    Auto stash before merge of "master" and "origin/master"

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ require('express-async-errors');
 require('./startup/db')();
 
 let app = express();
+
+require('./startup/db')();
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
