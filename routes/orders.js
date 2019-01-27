0a1621e8922a58559a555052a93311b20ab413f5
Jastria Rahmat <ijash1000@gmail.com>
2019-01-27 17:13:52 +0700

    basic routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,1 +1,11 @@
+const auth = require('../middleware/auth');
 const express = require('express');
+const router = express.Router();
+
+router.get('/', /* auth, */ async (req, res) => {
+    // TO DO...........
+    console.log('fill in with task....')
+    res.send('work in progress..........')
+});
+
+module.exports = router; 
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const auth = require('../routes/auth');
+const designers = require('../routes/designers');
+const items = require('../routes/items');
+const orders = require('../routes/orders');
 const users = require('../routes/users');
 
 module.exports = function (app) {
     app.use(express.json());
+    app.use('/api/auth', auth);
+    app.use('/api/designers', designers);
+    app.use('/api/items', items);
+    app.use('/api/orders', orders);
     app.use('/api/users', users);
 }
