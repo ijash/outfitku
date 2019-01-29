f2a5182db0c906ff8e97354a46d974196105cafd
Unknown <shirleyarianto24@gmail.com>
2019-01-29 18:55:13 +0700

    create post for item and merge category in item and designer

--- models/category.js
+++ models/category.js
@@ -4,22 +4,22 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  }
 });
 
 const Category = mongoose.model('Category', categorySchema);
 
 function validateCategory(category) {
-    const schema = {
-        name: Joi.string().min(3).max(35).required()
-    };
+  const schema = {
+    name: Joi.string().min(3).max(50).required()
+  };
 
-    return Joi.validate(category, schema);
+  return Joi.validate(category, schema);
 }
 
 exports.categorySchema = categorySchema;
