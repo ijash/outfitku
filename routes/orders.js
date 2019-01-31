9834d26e8657542e1ff60cc5a01d4af148af94a9
Jastria Rahmat <ijash1000@gmail.com>
2019-01-31 19:18:11 +0700

    improved orders routing.

--- routes/orders.js
+++ routes/orders.js
@@ -17,10 +17,7 @@ router.get('/', auth, async (req, res) => {
 router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-
     //get user
-
-
     const user = await User.findById(req.user._id).select('name');
     //check user
     if (!user) return res.status(404).send("user not found");
@@ -43,30 +40,50 @@ router.post('/', auth, async (req, res) => {
     res.send(order)
 });
 
-router.delete('/', auth, async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
     // find user
     // validate user
     // find designer
     // validate designer
     // find order
     // validate designer
-    // validate user by comparing with current order.user OR current designer.owner
-    // delete order
+    // validate user by comparing with current order.user OR current designer.owner to see if the user is the owner or buyer. others can't see or modify.
+
+    // edit the order
+
     // send status
-});
-
-router.put('/', auth, async (req, res) => {
-    // TO DO...........
-    console.log('fill in with task....')
     res.send('work in progress..........')
 });
 
+router.put('/:id', auth, async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-router.get('/:id', auth, async (req, res) => {
-    // TO DO...........
-    console.log('fill in with task....')
-    res.send('work in progress..........')
+    // find user
+    const user = await User.findById(req.user._id).select('name');
+    // validate user
+    if (!user) return res.status(404).send("user not found");
+    // find designer
+    const designer = await Designer.findById(req.body.designer).select('name account.owner._id');
+    // validate designer
+    if (!designer) return res.status(404).send('designer not found');
+    // find order
+    const order = await Order.findById(req.params._id)
+    // validate order
+    if (!order) return res.status(404).send("order not found");
+    // validate user by comparing with current order.user OR current designer.owner to see if the user is the owner or buyer. others can't see or modify.
+    if (!designer.user._id.equals(user._id) | !designer.account.owner._id.equals(user._id)) return res.status(404).send('unauthorized to modify this order.');
+
+    if (designer.user._id.equals(user._id)) {
+        //TO DO: things to do as buyer
+        res.send('data for user');
+    }
+    if (designer.account.owner._id.equals(user._id)) {
+        //TO DO: things to do as owner
+        res.send('data for owner');
+    }
+    // send status
+
 });
-
 
 module.exports = router; 
